ec95a44036f90e8694e919bbdeaf0f79251c0b73
= <F.Gutierrez@artechnw.com>
2021-08-24 22:55:02 -0700

    file generated by the script main.sh

---
+++ terraform/vars.tf
@@ -0,0 +1,4 @@
+variable "name"{
+type = list(string)
+default = ["juan.sanchez","jose.gomez","miguel.diaz","alonso.ortiz","francisco.gomez","hector.zavala","jose.sanchez","marco.lopez","agustin.carrillo","juan.arellano","antonio.lozano"]
+}
