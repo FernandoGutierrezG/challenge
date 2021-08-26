6665bd9f5eaf721f89888df88dc24247cd14b18c
${USERNAME} <${MAIL}>
2021-08-26 18:13:49 +0000

    update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
 variable "name"{
 type = list(string)
-default = ["juan.sanchez","jose.gomez","miguel.diaz","alonso.ortiz","francisco.gomez","hector.zavala","jose.sanchez","marco.lopez","agustin.carrillo","juan.arellano","antonio.lozano"]
+default = ["juan.sanchez","jose.gomez","miguel.diaz","alonso.ortiz","francisco.gomez","hector.zavala","jose.sanchez","marco.lopez","agustin.carrillo","juan.arellano","antonio.lozano","saul.arambula","victor.gutierrez","moises.mendez","alfonso.cabrera","misael.rendon"]
 }
